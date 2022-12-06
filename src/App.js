8d405cf2e0d941f0ac635e8af5edd47d5a046c3c
Meghan Waiss <mjwaiss@gmail.com>
2022-12-05 21:21:18 -0800

    formatted API output and added basic CSS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [data, setData] = useState("");
 
   useEffect(() => {
-    fetch('https://classproxy.rtoal.repl.co/meghan?location=Boston')
+    fetch('https://classproxy.rtoal.repl.co/meghan?location="Los Angeles"')
       .then((r) => r.json())
       .then((r) => setData(r))
       .catch((e) => setData(e));
@@ -14,7 +14,31 @@ export default function App() {
 
   return (
     <div className="App">  
-       {JSON.stringify(data)}
+      <section>
+        {
+          !data ? <li>No businesses yet</li> :
+
+          data?.businesses?.map(business =>
+            <div className="business" key={business.id}>
+              <div>
+                <div className="business_name">{business.name}</div>
+                <div>{business.price}</div>
+                <div className="business_address">
+                  <div>{business.location.address1}</div>
+                  <div>{business.location.address2}</div>
+                  <div>{business.location.address3}</div>
+                  <div>{business.location.city}</div>
+                </div>
+                <div>{business.display_phone}</div>
+                <div>Rating: {business.rating}/5</div>
+              </div>
+              <div className="business_image">
+                <img src={business.image_url} width="125"/>
+              </div>
+            </div>
+          )
+        }
+      </section>
     </div>
   )
 
