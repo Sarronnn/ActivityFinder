69a252195c42dab7260f41306048834aefd990cb
Ray Toal <rtoal@lmu.edu>
2022-12-19 20:29:33 -0800

    Get favorites to be fetched and displayed

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,23 +1,19 @@
 import { fetchFavorites } from "../services/favoritesService";
-import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
-import SearchBar from "../SearchBar";
 import { SignIn, SignOut, useAuthentication } from "../services/authService";
-import Card from "../components/Card";
 
 export default function Favorites(props) {
   const [data, setData] = useState("");
-  const [name, setName] = useState("");
   const user = useAuthentication();
 
   useEffect(() => {
-    const getData=async()=>{
-        const fetchedData=await fetchFavorites();
-        setData(fetchedData);
-        }
-        getData();
-
-  }, [name])
+    const getData = async () => {
+      const fetchedData = await fetchFavorites();
+      setData(fetchedData);
+      console.log(fetchedData);
+    };
+    getData();
+  }, []);
 
   return (
     <div className="App">
@@ -27,20 +23,22 @@ export default function Favorites(props) {
       </header>
       {user && (
         <div>
-          <SearchBar action={setName} />
-
           <section className="cards-container">
             {!data ? (
               <li>No businesses yet</li>
             ) : (
-              data?.businesses?.map((business) => (
-                <Card business={business} key={business.id} />
-              ))
+              <div>
+                <p>You favorited:</p>
+                <ul>
+                  {data.map((business) => (
+                    <li key={business.id}>{business.name}</li>
+                  ))}
+                </ul>
+              </div>
             )}
           </section>
         </div>
       )}
-      
     </div>
   );
 }
--- src/services/favoritesService.js
+++ src/services/favoritesService.js
@@ -1,20 +1,26 @@
-import { db } from "../firebaseConfig"
-import { collection, query, getDocs, addDoc, orderBy } from "firebase/firestore"
+import { db } from "../firebaseConfig";
+import {
+  collection,
+  query,
+  getDocs,
+  addDoc,
+  orderBy,
+} from "firebase/firestore";
 
 export async function writeFavoriteToDatabase({ name }) {
-    alert(`Saving Activity`)
-    const activityInfo = { name }
-    const docRef = await addDoc(collection(db, "favorites"), activityInfo)
-    return { id: docRef.id, ...activityInfo}
+  alert(`Saving Activity`);
+  const activityInfo = { name };
+  const docRef = await addDoc(collection(db, "favorites"), activityInfo);
+  return { id: docRef.id, ...activityInfo };
 }
 
 export async function fetchFavorites() {
-    const snapshot = await getDocs(
-        query(collection(db, "favorites"), orderBy("name", "desc"))
-    )
-console.log(snapshot)
-    return snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-    }))
+  const snapshot = await getDocs(
+    query(collection(db, "favorites"), orderBy("name", "desc"))
+  );
+  console.log(snapshot);
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
 }
